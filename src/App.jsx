a138bd7acda551fae59f927a9a081eaf9d47859a
MichaelTetris <125746441+MichaelTetris@users.noreply.github.com>
2024-10-14 16:50:36 +0300

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ImagesList from "./components/ImageGallery/ImageGallery";
+import Loader from "./components/Loader/loader";
 
 const api_url = "https://api.unsplash.com/search/photos";
 const IMAGES_PER_PAGE = 16;
@@ -11,8 +12,10 @@ function App() {
   /* console.log("key", import.meta.env.VITE_API_KEY); */
 
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (searchTerm) => {
+    setIsLoading(true);
     try {
       const response = await axios.get(`${api_url}`, {
         params: {
@@ -25,13 +28,15 @@ function App() {
       console.log(response);
     } catch (error) {
       console.error("Error fetching data from Unsplash", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div className="div_head">
       <SearchBar onClick={handleSubmit} />
-      {images.length > 0 && <ImagesList images={images} />}
+      {isLoading ? <Loader /> : images.length > 0 && <ImagesList images={images} />}
     </div>
   );
 }
